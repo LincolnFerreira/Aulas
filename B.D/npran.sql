823ab2055ece033abc49f41a46172fa08371a2ee
Lincoln Ferreira de Jesus do Carmo <lincoln.carmo@sptech.school>
2022-05-03 00:32:51 -0300

    atualizaçã ode pratica 8

--- B.D/npran.sql
+++ B.D/npran.sql
@@ -21,14 +21,14 @@ insert into cliente (nome,email,endereco) values
 
 alter table cliente add fkIndicador int, add foreign key (fkIndicador) references cliente(idCliente);
 
-insert into cliente (fkIndicador) values 
-    ('7'),
-    ('6'),
-    ('5'),
-    ('1'),
-    ('3'),
-    ('2'),
-    ('1');
+update cliente set fkIndicador = 7 where idCliente = 1;
+update cliente set fkIndicador = 6 where idCliente = 2;
+update cliente set fkIndicador = 5 where idCliente = 3;
+update cliente set fkIndicador = 4 where idCliente = 1;
+update cliente set fkIndicador = 3 where idCliente = 5;
+update cliente set fkIndicador = 2 where idCliente = 6;
+update cliente set fkIndicador = 1 where idCliente = 7;
+
 
 create table venda(
     idVenda int PRIMARY KEY AUTO_INCREMENT,
@@ -55,21 +55,21 @@ insert into venda (total,dtVenda) values
 select * from venda;
 alter table venda add fkCliente int, add foreign key (fkCLiente) references cliente(idCliente);
 
-insert into venda (fkCliente) values
-    ('1'),
-    ('2'),
-    ('3'),
-    ('4'),
-    ('5'),
-    ('6'),
-    ('7'),
-    ('7'),
-    ('6'),
-    ('5'),
-    ('4'),
-    ('3'),
-    ('2'),
-    ('1');
+update venda set fkCliente = 7 where idVenda = 1;
+update venda set fkCliente = 6 where idVenda = 2; 
+update venda set fkCliente = 5 where idVenda = 3; 
+update venda set fkCliente = 4 where idVenda = 4; 
+update venda set fkCliente = 3 where idVenda = 5; 
+update venda set fkCliente = 2 where idVenda = 6; 
+update venda set fkCliente = 1 where idVenda = 7; 
+update venda set fkCliente = 1 where idVenda = 8; 
+update venda set fkCliente = 2 where idVenda = 9; 
+update venda set fkCliente = 3 where idVenda = 10;
+update venda set fkCliente = 7 where idVenda = 11; 
+update venda set fkCliente = 4 where idVenda = 12; 
+update venda set fkCliente = 5 where idVenda = 13; 
+update venda set fkCliente = 5 where idVenda = 14; 
+
 
 create table produto(
     idProduto int PRIMARY KEY AUTO_INCREMENT,
@@ -102,4 +102,55 @@ create table NotaFiscal(
     primary key (idNotaFiscal, fkVenda, fkProduto)
 );
 
-select cliente.nome , venda.* from cliente join venda on fkCliente = idCliente; 
+insert into NotaFiscal (idNotaFiscal, fkVenda, fkProduto) values 
+	('1','12','2'),
+	('2','11','3'),
+	('3','10','4'),
+	('4','9','5'),
+	('5','8','6'),
+	('6','7','7'),
+	('7','6','8'),
+	('8','5','9'),
+	('9','4','10'),
+	('10','3','11'),
+	('11','2','12'),
+	('12','1','13'),
+	('13','12','13'),
+	('14','11','10'),
+	('15','10','9'),
+	('16','9','8'),
+	('17','8','7'),
+	('18','7','6'),
+	('19','6','5'),
+	('20','5','4'),
+	('21','4','3'),
+	('22','3','2'),
+	('23','3','2'),
+	('24','3','2'),
+	('25','3','2'),
+	('26','3','2'),
+	('27','3','2'),
+	('28','3','2'),
+	('29','2','1'),
+	('30','1','7');
+    
+    
+select * from cliente;
+select * from produto;
+select * from NotaFiscal;
+select * from venda;
+
+select cliente.nome, dtVenda, venda.total from cliente join venda on fkCliente = idCliente; 
+
+select cliente.nome, venda.* from cliente join venda on fkCliente = idCliente where idCliente = 2;
+
+select cliente.nome, indicador.nome from cliente join cliente as indicador on cliente.idCliente = indicador.fkIndicador; 
+
+select cliente.nome, indicador.nome from cliente join cliente as indicador on cliente.idCliente = indicador.fkIndicador where indicador.idCliente = 3;
+
+select cliente.nome, indicador.nome as indicador, venda.dtVenda, venda.total, produto.nome, produto.descricao, produto.preco from cliente left join cliente as indicador on cliente.idCliente = indicador.fkIndicador join venda on cliente.idCliente = fkCliente join notaFiscal on fkVenda = idVenda  join produto on idProduto = fkProduto;
+
+select venda.dtVenda, produto.nome from venda join NotaFiscal on idVenda = fkVenda join produto on idProduto = fkProduto where idVenda = 2; 
+
+select produto.nome, produto.preco from produto;
+
